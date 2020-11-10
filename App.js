d3ea69a4858947cf069fdecf519cbe1f85f16dd3
David Sheinbein <davesheinbein@gmail.com>
2020-11-10 14:46:43 -0800

    update to line graph

--- App.js
+++ App.js
@@ -5,17 +5,91 @@ import {
 	TextInput,
 	View,
 	SafeAreaView,
+	Dimensions,
 	Button,
 } from 'react-native';
 // import Todo from './components/Todo';
+import {
+	BarChart,
+	LineChart,
+} from 'react-native-chart-kit';
+import moment from 'moment';
 
 const App = () => {
 	const [description, setDescription] = useState('');
 	const [amount, setAmount] = useState('');
 	const [total, setTotal] = useState(0);
-	const [gigs, setGigs] = useState([
-		{ description: 'Freelancing job', amount: 599.99 },
+	const [data, setData] = useState([
+		{
+			date: moment().format('LL'),
+			amount: 2000,
+		},
+		{
+			date: moment().subtract(1, 'days').format('LL'),
+			amount: 2500,
+		},
+		{
+			date: moment().subtract(2, 'days').format('LL'),
+			amount: 3500,
+		},
+		{
+			date: moment().subtract(3, 'days').format('LL'),
+			amount: 5500,
+		},
+		{
+			date: moment().subtract(4, 'days').format('LL'),
+			amount: 1500,
+		},
 	]);
+	const [tranformedData, setTransformedData] = useState([]);
+	const [gigs, setGigs] = useState([
+		{
+			description: 'Freelancing job',
+			amount: 599.99,
+			timestamp: new Date(),
+		},
+	]);
+
+	const groupBy = (array, key) =>
+		array.reduce((rv, x) => {
+			(rv[x[key]] = rv[x[key]] || []).push(x);
+			return rv;
+		}, {});
+
+	const getDates = () =>
+		tranformedData.map((pair) => pair.date);
+	const getAmounts = () =>
+		tranformedData.map((pair) => pair.amount);
+
+	const transformData = (groupedData) => {
+		const transformedArray = [];
+		Object.entries(groupedData).forEach((entry) => {
+			// sums ups each day entries
+			const total = entry[1].reduce(
+				(total, pair) => total + pair.amount,
+				0
+			);
+			transformedArray.push({
+				date: entry[0],
+				amount: total,
+			});
+		});
+		return transformedArray;
+		console.log(transformedArray, '<< transformedArray');
+	};
+
+	// console.log(data, '<< data');
+	// console.log(getAmounts(), '<< getAmounts');
+	// console.log(getDates(), '<< getDates');
+	// console.log(groupBy(data, 'date'), '<< groupBy');
+	// console.log(
+	// 	Object.entries(data, 'date'),
+	// 	'<< Object.entries(data)'
+	// );
+	console.log(
+		transformData(groupBy(data, 'date')),
+		'<<< transformData(groupBy(data, date)'
+	);
 
 	const addGig = () => {
 		setGigs([
@@ -23,6 +97,7 @@ const App = () => {
 			{
 				description: description,
 				amount: amount,
+				timestamp: new Date(),
 			},
 		]);
 
@@ -31,19 +106,73 @@ const App = () => {
 	};
 
 	useEffect(() => {
+		setTransformedData(
+			transformData(groupBy(data, 'date'))
+		);
+		return () => {
+			console.log('<< Hitting use effect');
+		};
+	}, [data]);
+
+	useEffect(() => {
 		setTotal(
 			gigs.reduce(
 				(total, gig) => total + Number(gig.amount),
 				0
 			)
 		);
-		return console.log('hitting use effect');
+		// return console.log('<<< hitting use effect');
 	}, [gigs]);
 
 	return (
 		<SafeAreaView style={styles.container}>
 			<View>
-				<Text style={styles.titleText}>Time Tracker</Text>
+				<Text style={styles.titleText}>
+					- Money Tracker -
+				</Text>
+			</View>
+			<View>
+				<Text style={styles.subTitleText}>
+					Monthly Earnings Chart
+				</Text>
+				<LineChart
+					data={{
+						labels: getDates(),
+						datasets: [
+							{
+								data: getAmounts(),
+							},
+						],
+					}}
+					width={Dimensions.get('window').width} // from react-native
+					height={220}
+					yAxisLabel='$'
+					// yAxisSuffix='K'
+					yAxisInterval={1} // optional, defaults to 1
+					chartConfig={{
+						backgroundColor: '#e26a00',
+						backgroundGradientFrom: '#4CA1AF',
+						backgroundGradientTo: '#C4E0E5',
+						decimalPlaces: null, // optional, defaults to 2dp
+						color: (opacity = 1) =>
+							`rgba(50, 50, 50, ${opacity})`,
+						labelColor: (opacity = 1) =>
+							`rgba(50, 50, 50, ${opacity})`,
+						style: {
+							borderRadius: 16,
+						},
+						propsForDots: {
+							r: '4',
+							strokeWidth: '1.5',
+							stroke: '#ffffff',
+						},
+					}}
+					bezier
+					style={{
+						marginVertical: 8,
+						borderRadius: 16,
+					}}
+				/>
 			</View>
 			<Text>Total Income: ${total}</Text>
 			<TextInput
@@ -84,6 +213,13 @@ const styles = StyleSheet.create({
 		fontSize: 30,
 		fontWeight: 'bold',
 		textAlign: 'center',
+	},
+	subTitleText: {
+		fontSize: 12,
+		fontWeight: 'bold',
+		textAlign: 'left',
+		marginHorizontal: 10,
+		marginTop: 10,
 	},
 	textInput: {
 		height: 40,
